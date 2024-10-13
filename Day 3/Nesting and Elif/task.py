6945a3b3955d23dcfb6128d7776c58560cb386d8
sidcupmc <sidcupmc@outlook.com>
2024-10-13 15:30:15 +0100

    Rollercoaster height and age checker

--- Day 3/Nesting and Elif/task.py
+++ Day 3/Nesting and Elif/task.py
@@ -1,16 +1,26 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
+
 
 if height >= 120:
     print("You can ride the rollercoaster")
     age= int(input("What is your age"))
 
     if age <= 12:
-        print("Sorry, you are too young")
+        print("Child tickets are £5")
+        bill= 5
 
     elif age <= 18:
-        print("You have to pay £5")
+        print("Youth tickets are £7")
+        bill = 7
     else:
-        print("You have to pay £8")
+        print("Adult tickets are £12")
+        bill = 12
+
+    wantsPhoto = input("Do you want a photo taken? y for yes, n for No")
+    if wantsPhoto == "y":
+        bill += 3
+    print(f"Your total bill is £{str(bill)}" )
 else:
     print("Sorry you have to grow taller before you can ride.")
