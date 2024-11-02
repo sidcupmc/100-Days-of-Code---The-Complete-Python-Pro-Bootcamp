6926a0c8a054c4d80bb56c2323c0f394459ca8b6
sidcupmc <sidcupmc@outlook.com>
2024-11-02 18:03:12 +0000

    Just continuing with training course

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -33,26 +33,26 @@ def choose_winner(human_int, computer_int):
     if human_int == computer_int:
         return "It's a draw!"
     elif human_int == 0 and computer_int == 2:
-        return "human wins"
+        return "You win"
     elif human_int == 1 and computer_int == 0:
-        return "human wins"
+        return "You win"
     elif human_int == 2 and computer_int == 1:
-        return "human wins"
+        return "You win"
     else:
-        return "computer wins"
+        return "You lose"
 
 choices = [rock, paper, scissors]
 
 human_player_int = int(input("What do you choose? 0 for Rock, 1 for Paper, 2 for Scissors\n"))
-print(human_player_int)
+#print(human_player_int)
 human_player_choice = choices[human_player_int]
 
 computer_player_int = random.randint(0,2)
-print(computer_player_int)
+#print(computer_player_int)
 computer_player_choice = choices[computer_player_int]
 
-print("You chose " + str(human_player_int) + " Which is "  +  "\n" + human_player_choice)
+print("You chose " + human_player_choice)
 
-print("The computer chose " + str(computer_player_int) + " Which is "  +  "\n" + computer_player_choice)
+print("The computer chose " + computer_player_choice)
 
 print(choose_winner(human_player_int, computer_player_int))
