0f21755205cce0f67c2a6ec1b4e4407beaa2a182
sidcupmc <sidcupmc@outlook.com>
2024-11-11 19:42:14 +0000

    Just continuing with training course

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -10,29 +10,21 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 
-
-letters_list = []
-number_list = []
-symbols_list = []
-
+password_list = []
 
 for item in range(0,nr_letters):
-    letters_list.append(random.choice(letters))
-print(letters_list)
+    password_list.append(random.choice(letters))
 
 for item in range(0,nr_numbers):
-    number_list.append(random.choice(numbers))
-print(number_list)
+    password_list.append(random.choice(numbers))
 
 for item in range(0,nr_symbols):
-    symbols_list.append(random.choice(symbols))
-print(symbols_list)
+    password_list.append(random.choice(symbols))
 
-ordered_password_list = letters_list + number_list + symbols_list
-print(ordered_password_list)
+print(password_list)
 
-random.shuffle(ordered_password_list)
-print(ordered_password_list)
+random.shuffle(password_list)
+print(password_list)
 
 
 
