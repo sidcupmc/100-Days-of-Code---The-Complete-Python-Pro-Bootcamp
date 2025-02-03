7bb5804bd7aacd98238db64cbf7e6e6bb0cf2e43
sidcupmc <sidcupmc@outlook.com>
2025-02-03 21:37:10 +0000

    Just continuing with training course

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -8,15 +8,22 @@ print(chosen_word)
 placeholder = ''
 for each_letter in chosen_word:
     placeholder += '_'
+
 print(placeholder + '\n')
+display = placeholder
 
 guess = input("Guess a letter: ").lower()
 
 # TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
 
+index = 0
 for letter in chosen_word:
     if letter == guess:
+        display = display[:index] + letter + display[index + 1:]
         print("Right")
     else:
-        print("Wrong")
 
+        print("Wrong")
+    index += 1
+
+print(display)
